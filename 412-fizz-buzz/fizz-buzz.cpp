3919dd6fc3bc01e0023d47c741388105434e3a01
Mudassar <mudassarkareem916@gmail.com>
2024-06-06 08:29:30 +0500

    Time: 0 ms (100.00%) | Memory: 10 MB (68.14%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -4,11 +4,10 @@ public:
         vector<string> answer;
         for(int i =1; i<=n; i++){
             if(i%3 == 0 && i%5 == 0) answer.push_back("FizzBuzz");
-            else if(i%3 == 0) answer.push_back("Fizz");
-            else if (i%5 == 0) answer.push_back("Buzz");
+            else if(i%3 ==0) answer.push_back("Fizz");
+            else if(i%5 == 0) answer.push_back("Buzz");
             else answer.push_back(to_string(i));
-        }
-        return answer;
-        
+        };
+        return answer; 
     }
 };
