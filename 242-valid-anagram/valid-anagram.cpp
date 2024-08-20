7c0ebb0a71c38604e125455324cbd497bceee178
Mudassar <mudassarkareem916@gmail.com>
2024-08-20 10:05:38 +0500

    Time: 8 ms (63.58%) | Memory: 8.9 MB (41.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s==t;
+        if(s.size() != t.size()){
+            return false;
+        };
+        vector<int> countS(26,0);
+        vector<int> countT(26,0);
+        for(int i=0; i<s.size(); i++){
+            countS[s[i]-'a']++;
+            countT[t[i]-'a']++;
+        };
+        return countS == countT;
     }
 };
