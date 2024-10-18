5223e54973929b34d3746a042a131751bc8a2c03
Mudassar <mudassarkareem916@gmail.com>
2024-10-18 11:35:38 +0500

    Time: 0 ms (100.00%) | Memory: 9 MB (45.69%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int good = 0;
-        for(int i =0; i< nums.size()-1; i ++){
-            for(int j = i +1; j < nums.size() ; j++){
-                if(nums [i] == nums[j]){
-                    good++;
-                };
-            };
-        };
-        return good;
+        int count =0;
+        for(int i=0 ; i<nums.size(); i++){
+            for(int j=1; j<nums.size(); j++){
+                if( nums[i] == nums[j] & i<j ){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 };
