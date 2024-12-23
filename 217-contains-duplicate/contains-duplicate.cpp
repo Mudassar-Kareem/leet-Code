13e8317ded8a3015e3b26f7cb4f6cf2da92f3233
Mudassar <mudassirhussain@giccl.edu.pk>
2024-12-23 14:56:00 +0500

    Time: 25 ms (78.38%) | Memory: 73.5 MB (36.90%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> hashSet;
-        for(int num :nums){
-            if(hashSet.find(num) != hashSet.end()){
+        unordered_set<int> seen;
+        for(int num : nums){
+            if(seen.count(num)){
                 return true;
             }
-            hashSet.insert(num);
+            seen.insert(num);
         }
         return false;
     }
