116ee220edd3d39473697d7c3407cc35f239e499
Mudassar <mudassirhussain@giccl.edu.pk>
2024-12-24 11:12:11 +0500

    Time: 3 ms (61.97%) | Memory: 9.9 MB (33.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -3,13 +3,13 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size() != t.size()){
             return false;
-        };
-        vector<int> countS(26,0);
-        vector<int> countT(26,0);
-        for(int i=0; i<s.size(); i++){
-            countS[s[i]-'a']++;
-            countT[t[i]-'a']++;
-        };
-        return countS == countT;
+        }
+        unordered_map<char,int> Smap;
+        unordered_map<char,int> Tmap;
+        for(int i=0; i< s.size(); i++){
+            Smap[s[i]] ++;
+            Tmap[t[i]]++;
+        }
+        return Smap == Tmap ;
     }
 };
