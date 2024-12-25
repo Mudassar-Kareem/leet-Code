e081fae3827b07bfd07c98e229bd4bf7987c9a24
Mudassar <mudassirhussain@giccl.edu.pk>
2024-12-25 11:41:59 +0500

    Time: 0 ms (100.00%) | Memory: 14.9 MB (21.73%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int> mymap;
-        for(int i =0; i< nums.size() ; i++){
-            int reminder = target- nums[i];
-            if(mymap.count(reminder)){
-                return {i, mymap[reminder]};
-            };
-            mymap[nums[i]]=i;
-        };
+        unordered_map<int,int> map;
+        for(int i=0; i< nums.size(); i++){
+            int reminder =  target - nums[i];
+            if(map.find(reminder) != map.end()){
+                return {map[reminder], i};
+            }
+            map[nums[i]] =i;
+        }
         return {-1,-1};
-        
     }
 };
