f0225d1c060b4ee9af291380fb3d7fd7a5e29771
Mudassar <mudassirhussain@giccl.edu.pk>
2025-01-06 15:02:57 +0500

    Time: 0 ms (100.00%) | Memory: 8.9 MB (26.92%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> s1;
-        for(char c:s){
-            if(c =='(' || c== '{' || c== '['){
-                s1.push(c);
+        stack<char> st;
+        for( auto c : s){
+            if(c == '(' || c == '{' || c == '['){
+                st.push(c);
             }else{
-                if(s1.empty()) return false;
-                char top = s1.top();
+                if(st.empty()) return false;
+                char top = st.top();
                 if((c == ')' && top != '(')||(c == '}' && top != '{')||(c == ']' && top != '[')){
                     return false;
                 }
-                s1.pop();
+                st.pop();
             }
         }
-        return s1.empty();
+        return st.empty();
     }
 };
